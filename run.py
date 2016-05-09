a189f0bed16064a0c31cea58f1a8806e6d3b14c8
kimmytaft <kimmy7836@gmail.com>
2016-05-09 15:02:56 -0700

    uses for loop to display multiple pictures on same webpage

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 from os import listdir
+
 app = Flask(__name__)
 
 @app.route("/")
 def template_test():
     pic_list = listdir("static/")
-    return render_template('displaypic.html', picture_list=pic_list)
+    return render_template('displaypic.html', pic_list=pic_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
