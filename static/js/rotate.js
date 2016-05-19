561303f1180c552c231135edf8a1fb1d9fcc9c25
kimmytaft <kimmy7836@gmail.com>
2016-05-19 15:27:25 -0700

    pictures now rotate in an infinite loop

--- static/js/rotate.js
+++ static/js/rotate.js
@@ -1,2 +1,16 @@
 var body = document.getElementsByTagName("body")[0];
-body.style.backgroundImage = "url('/static/pictures/singapore.png')";
+var path = "/static/pictures/";
+var allPics = ['pookie.jpeg', 'pool.jpg', 'sea_urchin.tiff', 'singapore.png', 'snake.png', 'standing_buddha.jpg'];
+var i = 0;
+var intervalID = window.setInterval(slideshow, 5000);
+
+function slideshow() {
+    var pic = "url(" + path + allPics[i] + ")";
+    body.style.backgroundImage = pic;
+    if(i == (allPics.length -1)){
+        i = 0;
+    }
+    else{
+        i++;
+    }
+}
