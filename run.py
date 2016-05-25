dd36df4e81f74c85d68ee94db68da9b02951d46a
kimmytaft <kimmy7836@gmail.com>
2016-05-25 16:13:38 -0700

    add installed packages to requirements.txt, use relative path instead of absolute, consolidate functions to one upload function

--- run.py
+++ run.py
@@ -1,28 +1,27 @@
 import os
 from werkzeug import secure_filename
-from flask import Flask, render_template, session, g, abort, flash, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 
 app = Flask(__name__)
 
-app.config['UPLOAD_FOLDER'] = '/home/taftk2/CS_Support/jinja/static/pictures'
-app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg', 'tiff', 'gif'])
+app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(__file__), 'static/pictures')
+app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'tiff', 'gif'}
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
-@app.route("/upload")
-def upload_file():
-    return render_template('upload.html')
-
-@app.route("/upload", methods=['POST'])
+@app.route("/upload", methods=['POST', 'GET'])
 def upload_picture():
-    pic=request.files['file']
-    if pic and allowed_file(pic.filename):
-        filename = secure_filename(pic.filename)
-        pic.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return redirect(url_for('uploaded_file', filename=filename))
+    if request.method == 'POST':
+        pic=request.files['file']
+        if pic and allowed_file(pic.filename):
+            filename = secure_filename(pic.filename)
+            pic.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+        else:
+            return render_template('extension_error.html', extensions=app.config['ALLOWED_EXTENSIONS'])
     else:
-        return render_template('extension_error.html', extensions=app.config['ALLOWED_EXTENSIONS'])
+        return render_template('upload.html')
 
 @app.route('/static/pictures/<filename>')
 def uploaded_file(filename):
@@ -34,4 +33,5 @@ def slideshow():
     return render_template('displaypic.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    debug = True if os.environ.get('SLIDESHOW_DEBUG') else False
+    app.run(debug=debug)
