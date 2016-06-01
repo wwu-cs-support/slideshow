d3a9f40cdb3b46636010f60712a213e614c71699
kimmytaft <kimmy7836@gmail.com>
2016-06-01 14:47:55 -0700

    cycles back to the beginning after timeout

--- static/js/rotate.js
+++ static/js/rotate.js
@@ -5,10 +5,20 @@ var timeout = 0;
 function displaySlide(pic) {
     let url = `url("${pic.path}")`;
     let body = document.getElementsByTagName('body')[0];
-    timeout = timeout + parseInt(pic.duration, 10);
     window.setTimeout(() => { body.style.backgroundImage = url; }, timeout);
+    timeout = timeout + parseInt(pic.duration, 10);
 }
 
+//Total time of slideshow.
+//Value returned is used to 'refresh' the page.
+function getTotalTimeout(metadata) {
+  let total = 0;
+  metadata.forEach( (pic) => {
+    total = total + parseInt(pic.duration);
+  });
+
+  return total;
+}
 
 function loadPictures(url) {
   let req = new XMLHttpRequest();
@@ -17,7 +27,9 @@ function loadPictures(url) {
       if (req.status === 200) {
         let metadata = JSON.parse(req.responseText);
         metadata.pictures.forEach(displaySlide);
-        loadPictures(url);
+        let totalTime = getTotalTimeout(metadata.pictures);
+        console.log(totalTime);
+        window.setTimeout(() => { loadPictures(url); }, totalTime);
       }
     }
   }
