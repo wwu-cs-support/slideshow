8eb7cad21326c47f7dbebf4ea7f6f38bb5aaefd8
kimmytaft <kimmy7836@gmail.com>
2017-02-02 15:05:51 -0800

    Add event listener for when a user changes the order of the slideshow. Prints the new order to the console.

--- static/js/reorder.js
+++ static/js/reorder.js
@@ -2,3 +2,25 @@ $(function() {
     $( "#sortable" ).sortable();
     $( "#sortable" ).disableSelection();
 });
+
+$(document).ready(function () {
+    var sortableList = $(".sortableList");
+
+    var sortEventHandler = function(event, ui){
+        console.log("New Sort order!");
+        sortableList = $(".sortableList");
+
+        var listElements = sortableList.children();
+
+        var listValues = [];
+        for (i = 0; i < listElements.length; i++){
+            listValues.push(listElements[i].innerHTML);
+        }
+        console.log(listValues);
+    };
+
+    sortableList.sortable({
+        stop: sortEventHandler
+    });
+
+});
